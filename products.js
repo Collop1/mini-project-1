72797cbb558fd7feb1b5e31d6f0fee0dce254943
Collop1 <collop111cop@gmail.com>
2025-06-07 11:36:05 +1200

    Add deploy.yml and add products file

---
+++ products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        "product_id": 100,
+        "thumbnail": "images/thumbnail-pepperoni.jpg",
+        "name": "Margherita Pizza",
+        "description": "Classic Margherita pizza with fresh basil and mozzarella cheese.",
+        "category": "Pizza",
+        "price": 34.99,
+        "created_at": "2024-12-01 04:21:45",
+        "isActive": true
+    },
+    {
+        "product_id": 101,
+        "thumbnail": "images/thumbnail-pepperoni.jpg",
+        "name": "Veggie Supreme Pizza",
+        "description": "A delightful mix of fresh vegetables on a cheesy base.",
+        "category": "Pizza",
+        "price": 39.99,
+        "created_at": "2024-12-12 02:19:15",
+        "isActive": true
+    },
+    {
+        "product_id": 102,
+        "thumbnail": "images/thumbnail-pepperoni.jpg",
+        "name": "BBQ Chicken Pizza",
+        "description": "BBQ chicken pizza with a smoky flavor and tangy sauce.",
+        "category": "Pizza",
+        "price": 29.99,
+        "created_at": "2024-15-19 06:21:45",
+        "isActive": true
+    },
+    {
+        "product_id": 103,
+        "thumbnail": "images/thumbnail-pepperoni.jpg",
+        "name": "Pepperoni Pizza",
+        "description": "Classic pepperoni pizza with a crispy crust and rich tomato sauce.",
+        "category": "Pizza",
+        "price": 19.99,
+        "created_at": "2025-03-23 04:21:45",
+        "isActive": false
+    },
+];
+
+export function getAllProducts() {
+    return products;
+}
